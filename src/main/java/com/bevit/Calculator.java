7cb03a1f0674e0549bc7b5536dc05433a995dd2d
Bevilaqua <filipe.s.bevilaqua@accenture.com>
2020-07-12 17:51:41 -0300

    Com Junit testando

---
+++ src/main/java/com/bevit/Calculator.java
@@ -0,0 +1,9 @@
+package com.bevit;
+
+public class Calculator {
+
+    public double somar(double a, double b){
+        return a+b;
+    }
+    
+}
---
+++ src/test/java/com/bevit/CalculatorTeste.java
@@ -0,0 +1,15 @@
+package com.bevit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculatorTeste {
+    
+    @Test
+    public void TestarSoma(){
+        Calculator c = new Calculator();
+        double soma = 1+2;
+        assertEquals(3, soma, 0.01);
+    }
+}
